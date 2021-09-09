a9d2744d9e077b8694f3fe051558383ffc0b786e
Good Vibes <sethhixon1992@gmail.com>
2021-09-09 16:41:14 -0500

    added limit warning

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,4 @@
+var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -10,6 +11,10 @@ var getRepoIssues = function(repo) {
                 displayIssues(data);
             });
         }
+        // check if api has paginated issues
+        if(response.headers.get("Link")){
+            displayWarning(repo);
+        }
         else {
             alert("There was a problem with your request!");
         }
@@ -17,7 +22,7 @@ var getRepoIssues = function(repo) {
 };
 var displayIssues= function(issues) {
   if(issues.length === 0) {
-      issueContainerEl.textContent = "This repo has no open issues!");
+      issueContainerEl.textContent =("This repo has no open issues!");
       return;
   }
     for(var i = 0; i < issues.length; i++) {
@@ -48,6 +53,18 @@ var displayIssues= function(issues) {
       // append to container 
       issueEl.appendChild(typeEl);
   }
+};
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues visit";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on Github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container 
+    limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
