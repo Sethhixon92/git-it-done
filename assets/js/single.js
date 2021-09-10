5716a21514b695825a359cb979415ee913156029
Good Vibes <sethhixon1992@gmail.com>
2021-09-10 02:14:59 -0500

    Added error checking

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,23 @@
+var repoNameEl = document.querySelector("#repo-name");
 var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
+
+var getRepoName = function() {
+   // grab repo name from the url query string
+    var queryString= document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
    
@@ -9,15 +27,18 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data){
                 //pass response data to the dom function
                 displayIssues(data);
-            });
-        }
+        
+        
         // check if api has paginated issues
         if(response.headers.get("Link")){
             displayWarning(repo);
+         }
+        });
+        } else {
+           // if not successful return to homepage
+            document.location.replace("./index.html");
         }
-        else {
-            alert("There was a problem with your request!");
-        }
+       
     });
 };
 var displayIssues= function(issues) {
@@ -66,5 +87,4 @@ var displayWarning = function(repo) {
     // append to warning container 
     limitWarningEl.appendChild(linkEl);
 };
-
-getRepoIssues("facebook/react");
+getRepoName()
